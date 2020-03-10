bb78463436de33a21423430e3e739796cf94444f
chaitramallikarjun <chaitramallikarjun1994@gmail.com>
2020-03-10 08:06:55 -0700

    first commit

---
+++ src/main/java/AutoIt/AutoIt/App.java
@@ -0,0 +1,13 @@
+package AutoIt.AutoIt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/AutoIt/AutoIt/WindowHangleUsingURL.java
@@ -0,0 +1,23 @@
+package AutoIt.AutoIt;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHangleUsingURL {
+	
+	 public static void main( String[] args )
+	 {
+		
+
+	
+	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+	WebDriver  driver=new ChromeDriver();
+	//driver.get("https://the-internet.herokuapp.com/");
+	
+	driver.get("http://admin:admin@the-internet.herokuapp.com/");
+	driver.findElement(By.xpath("//a[text()='Basic Auth']")).click();
+		 
+	 }
+
+}
